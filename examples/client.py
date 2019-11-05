d4efec7d0dfc28da079860b9966f460af552cba3
linux_china <libing.chen@gmail.com>
2019-11-04 17:42:34 -0800

    add setup frame support

--- examples/client.py
+++ examples/client.py
@@ -5,6 +5,7 @@ import asyncio
 from reactivestreams import Subscriber
 from rsocket import Payload
 from rsocket import RSocket
+from rsocket.composite_metadata import CompositeMetadata
 
 
 class StreamSubscriber(Subscriber):
@@ -24,11 +25,16 @@ class StreamSubscriber(Subscriber):
 
 
 async def download(reader, writer):
-    socket = RSocket(reader, writer, server=False)
+    composite_metadata = CompositeMetadata()
+    composite_metadata.add_custom_metadata("application/info", b'{"id":1}')
+    socket = RSocket(reader, writer, server=False,
+                     data_encoding=b'text/plain', metadata_encoding=b'message/x.rsocket.composite-metadata.v0',
+                     setup_payload=Payload(b'', composite_metadata.get_source()))
     payload = Payload(b'The quick brown fox', b'meta')
     print('RR: {}'.format(await socket.request_response(payload)))
     socket.request_stream(payload).subscribe(StreamSubscriber())
-    await asyncio.sleep(0.1)
+    socket.fire_and_forget(Payload(b'fire_and_forget request', b'meta'))
+    await asyncio.sleep(1.0)
     await socket.close()
     return payload
 
