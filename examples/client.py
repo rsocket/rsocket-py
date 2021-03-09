721a00a2c4b561cee19a56284aa39ded5651a174
JIAWea <1552937000@qq.com>
2021-03-09 13:05:20 +0800

    update: examples

--- examples/client.py
+++ examples/client.py
@@ -2,33 +2,70 @@
 
 import asyncio
 
+import rxbp
+from rxbp.typing import ElementType
+
 from reactivestreams import Subscriber
 from rsocket import Payload
 from rsocket import RSocket
 
 
-class StreamSubscriber(Subscriber):
-    def on_next(self, value):
-        print('RS: {}'.format(value))
-        self.subscription.request(1)
+# class StreamSubscriber(Subscriber):
+#     def on_next(self, value):
+#         print('RS: {}'.format(value))
+#         self.subscription.request(1)
+#
+#     def on_complete(self):
+#         print('RS: Complete')
+#
+#     def on_error(self, exception):
+#         print('RS: error: {}'.format(exception))
+#
+#     def on_subscribe(self, subscription):
+#         # noinspection PyAttributeOutsideInit
+#         self.subscription = subscription
 
-    def on_complete(self):
-        print('RS: Complete')
+
+class Observer(Subscriber):
+    def on_next(self, elem: ElementType):
+        try:
+            values = list(elem)
+        except Exception as exc:
+            print("Exception: ", exc)
+        else:
+            for v in values:
+                data = v.data.decode('utf-8')
+                metadata = v.metadata.decode('utf-8')
+                print('Response from client: {}, {}'.format(data, metadata))
+            self.subscription.request(1)
 
     def on_error(self, exception):
-        print('RS: error: {}'.format(exception))
+        print('Exception: {}'.format(str(exception)))
+
+    def on_completed(self):
+        print('Completed!')
 
     def on_subscribe(self, subscription):
         # noinspection PyAttributeOutsideInit
         self.subscription = subscription
+        # self.subscription.request(20)
 
 
 async def download(reader, writer):
     socket = RSocket(reader, writer, server=False)
     payload = Payload(b'The quick brown fox', b'meta')
     print('RR: {}'.format(await socket.request_response(payload)))
-    socket.request_stream(payload).subscribe(StreamSubscriber())
-    await asyncio.sleep(0.1)
+    # socket.request_stream(payload).subscribe(StreamSubscriber())
+
+    socket.request_stream(payload).subscribe(Observer())
+
+    socket.request_channel(
+        rxbp.from_range(10, batch_size=1).pipe(
+            rxbp.op.map(lambda v: Payload(b'channel index-' + str(v).encode('utf-8'), b'm')),
+        )
+    ).subscribe(Observer())
+
+    await asyncio.sleep(10)
     await socket.close()
     return payload
 
--- examples/server.py
+++ examples/server.py
@@ -2,8 +2,40 @@
 
 import asyncio
 
+import rxbp
+from rxbp.schedulers.threadpoolscheduler import ThreadPoolScheduler
+
+from reactivestreams.publisher import Publisher
+from rxbp.flowable import Flowable
+from rxbp.typing import ElementType
+
+from reactivestreams import Subscriber
 from rsocket import RSocket, BaseRequestHandler
 from rsocket.payload import Payload
+
+
+class Observer(Subscriber):
+    def on_next(self, elem: ElementType):
+        try:
+            values = list(elem)
+        except Exception as exc:
+            print("Exception: ", exc)
+        else:
+            for v in values:
+                data = v.data.decode('utf-8')
+                metadata = v.metadata.decode('utf-8')
+                print('Response from client: {}, {}'.format(data, metadata))
+
+    def on_error(self, exception):
+        print('Exception: {}'.format(str(exception)))
+
+    def on_completed(self):
+        print('Completed!')
+
+    def on_subscribe(self, subscription):
+        # noinspection PyAttributeOutsideInit
+        self.subscription = subscription
+        self.subscription.request(20)
 
 
 class Handler(BaseRequestHandler):
@@ -14,12 +46,31 @@ class Handler(BaseRequestHandler):
             b'Escher are an artist.'))
         return future
 
+    def request_stream(self, payload: Payload) -> Flowable:
+        return rxbp.from_list([1, 3, 5, 7, 9], batch_size=1).pipe(
+            # rxbp.op.do_action(
+            #     on_next=lambda v: print("sending {}".format(v))),
+            rxbp.op.map(lambda v: Payload(b'stream-' + str(v).encode('utf-8'), b'm')),
+            rxbp.op.subscribe_on(pool),
+        )
+
+    def request_channel(self, publisher: Publisher) -> Flowable:
+        publisher.subscribe(Observer())
+        return rxbp.from_range(10, batch_size=1).pipe(
+            # rxbp.op.do_action(
+            #     on_next=lambda v: print("sending {}".format(v))),
+            rxbp.op.map(lambda v: Payload(b'channel-' + str(v).encode('utf-8'), b'm')),
+            rxbp.op.subscribe_on(pool),
+        )
+
 
 def session(reader, writer):
     RSocket(reader, writer, handler_factory=Handler)
 
 
 if __name__ == '__main__':
+    pool = ThreadPoolScheduler("")
+
     loop = asyncio.get_event_loop()
     service = loop.run_until_complete(asyncio.start_server(
         session, 'localhost', 9898))
