f4847fd61c883129915b171a51797657605eba45
gabi <gabis@precog.co>
2022-01-08 18:07:30 +0200

    fix failing tests

--- reactivestreams/publisher.py
+++ reactivestreams/publisher.py
@@ -1,5 +1,7 @@
 from abc import ABCMeta, abstractmethod
 
+from reactivestreams.subscription import Subscription
+
 
 class Publisher(metaclass=ABCMeta):
     @abstractmethod
@@ -7,6 +9,14 @@ class Publisher(metaclass=ABCMeta):
         ...
 
 
-class DefaultPublisher(Publisher):
+class DefaultPublisher(Publisher, Subscription):
+    async def request(self, n: int):
+        pass
+
+    def cancel(self):
+        pass
+
     def subscribe(self, subscriber):
+        self.subscriber = subscriber
+        self.subscriber.on_subscribe(self)
         subscriber.on_error(RuntimeError("Not implemented"))
