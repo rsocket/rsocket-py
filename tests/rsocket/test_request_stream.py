d81ed06361dc075914c06fbc0f6516ac3d3a0d93
gabi <gabis@precog.co>
2022-01-16 11:49:23 +0200

    added request stream with back-pressure test

--- tests/rsocket/test_request_stream.py
+++ tests/rsocket/test_request_stream.py
@@ -5,10 +5,12 @@ from typing import List
 import pytest
 
 from reactivestreams.publisher import Publisher
-from reactivestreams.subscriber import DefaultSubscriber
+from reactivestreams.subscriber import DefaultSubscriber, Subscriber
 from reactivestreams.subscription import DefaultSubscription
 from rsocket.payload import Payload
 from rsocket.request_handler import BaseRequestHandler
+from rsocket.rsocket_client import RSocketClient
+from rsocket.rsocket_server import RSocketServer
 
 
 @pytest.mark.asyncio
@@ -21,12 +23,15 @@ async def test_request_stream_not_implemented_by_server(pipe):
 
     with pytest.raises(RuntimeError):
         class Receiver(DefaultSubscriber):
-            def on_error(self, exception):
+            def on_error(self, exception: Exception):
                 error.set_exception(exception)
 
         error = asyncio.Future()
+
         client.request_stream(payload).subscribe(Receiver())
+
         await asyncio.wait_for(error, 0.25)
+
         not error.done() or error.exception()
 
 
@@ -143,59 +148,61 @@ async def test_request_stream_and_cancel_after_first_message(pipe):
     assert stream_subscriber.received_messages[0].data == b'Feed Item: 0'
 
 
-# @pytest.mark.asyncio
-# async def test_request_stream_with_back_pressure(pipe):
-#     server: RSocketServer = pipe[0]
-#     client: RSocketClient = pipe[1]
-#
-#     stream_canceled = asyncio.Event()
-#
-#     class Handler(BaseRequestHandler, Publisher, DefaultSubscription):
-#         def cancel(self):
-#             self.feeder.cancel()
-#             stream_canceled.set()
-#
-#         def subscribe(self, subscriber):
-#             subscriber.on_subscribe(self)
-#             self.feeder = asyncio.ensure_future(self.feed(subscriber))
-#
-#         async def request_stream(self, payload: Payload) -> Publisher:
-#             return self
-#
-#         @staticmethod
-#         async def feed(subscriber):
-#             loop = asyncio.get_event_loop()
-#             try:
-#                 for x in range(3):
-#                     value = Payload('Feed Item: {}'.format(x).encode('utf-8'))
-#                     await asyncio.sleep(1)
-#                     await subscriber.on_next(value)
-#                 loop.call_soon(subscriber.on_complete)
-#             except asyncio.CancelledError:
-#                 pass
-#
-#     class StreamSubscriber(DefaultSubscriber):
-#         def __init__(self):
-#             self.received_messages: List[Payload] = []
-#
-#         async def on_next(self, value, is_complete=False):
-#             self.received_messages.append(value)
-#             self.subscription.cancel()
-#             logging.info(value)
-#
-#         def on_complete(self, value=None):
-#             logging.info('Complete')
-#
-#         def on_subscribe(self, subscription):
-#             self.subscription = subscription
-#
-#     server.set_handler_using_factory(Handler)
-#
-#     stream_subscriber = StreamSubscriber()
-#
-#     client.request_stream(Payload(b'')).limit_rate(1).subscribe(stream_subscriber)
-#
-#     await stream_canceled.wait()
-#
-#     assert len(stream_subscriber.received_messages) == 1
-#     assert stream_subscriber.received_messages[0].data == b'Feed Item: 0'
+@pytest.mark.asyncio
+async def test_request_stream_with_back_pressure(pipe):
+    server: RSocketServer = pipe[0]
+    client: RSocketClient = pipe[1]
+    stream_completed = asyncio.Event()
+    requests_received = 0
+
+    class Handler(BaseRequestHandler, Publisher, DefaultSubscription):
+
+        def subscribe(self, subscriber: Subscriber):
+            subscriber.on_subscribe(self)
+            self._subscriber = subscriber
+            self._current_item = 0
+            self._max_items = 3
+
+        async def request(self, n: int):
+            nonlocal requests_received
+            requests_received += n
+
+            if self._current_item < self._max_items:
+                is_complete = self._current_item == self._max_items - 1
+                value = Payload('Feed Item: {}'.format(self._current_item).encode('utf-8'))
+                await self._subscriber.on_next(value, is_complete)
+                self._current_item += 1
+
+        async def request_stream(self, payload: Payload) -> Publisher:
+            return self
+
+    class StreamSubscriber(DefaultSubscriber):
+        def __init__(self):
+            self.received_messages: List[Payload] = []
+
+        async def on_next(self, value, is_complete=False):
+            self.received_messages.append(value)
+            await self.subscription.request(1)
+            logging.info(value)
+
+        def on_complete(self, value=None):
+            logging.info('Complete')
+            stream_completed.set()
+
+        def on_subscribe(self, subscription):
+            self.subscription = subscription
+
+    server.set_handler_using_factory(Handler)
+
+    stream_subscriber = StreamSubscriber()
+
+    client.request_stream(Payload(b'')).limit_rate(1).subscribe(stream_subscriber)
+
+    await stream_completed.wait()
+
+    assert len(stream_subscriber.received_messages) == 3
+    assert stream_subscriber.received_messages[0].data == b'Feed Item: 0'
+    assert stream_subscriber.received_messages[1].data == b'Feed Item: 1'
+    assert stream_subscriber.received_messages[2].data == b'Feed Item: 2'
+
+    assert requests_received == 3
