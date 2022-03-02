accebd71ce772207839f669b8a48f3f27db8ebbe
gabi <gabis@precog.co>
2022-03-02 09:16:16 +0200

    mark all methods in rsocket interface as abstract

--- rsocket/rsocket_interface.py
+++ rsocket/rsocket_interface.py
@@ -7,35 +7,46 @@ from rsocket.payload import Payload
 
 
 class RSocketInterface(metaclass=abc.ABCMeta):
+
+    @abc.abstractmethod
     def connect(self):
         ...
 
+    @abc.abstractmethod
     def request_channel(
             self,
             payload: Payload,
             local_publisher: Optional[Publisher] = None) -> Union[Any, Publisher]:
         ...
 
+    @abc.abstractmethod
     def register_stream(self, handler) -> int:
         ...
 
+    @abc.abstractmethod
     def send_frame(self, frame: Frame):
         ...
 
+    @abc.abstractmethod
     def send_complete(self, stream_id: int):
         ...
 
+    @abc.abstractmethod
     def finish_stream(self, stream_id: int):
         ...
 
+    @abc.abstractmethod
     def _log_identifier(self) -> str:
         ...
 
+    @abc.abstractmethod
     def send_request(self, frame: RequestFrame):
         ...
 
+    @abc.abstractmethod
     def send_payload(self, stream_id: int, payload: Payload, complete=False, is_next=True):
         ...
 
+    @abc.abstractmethod
     def send_error(self, stream_id: int, exception: Exception):
         ...
