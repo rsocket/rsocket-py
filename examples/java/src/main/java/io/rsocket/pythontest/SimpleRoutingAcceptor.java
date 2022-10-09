4a79904341beb4ffe563402ab8218a41da0827ea
gabi <gabis@precog.co>
2022-10-09 10:01:15 +0300

    fix fragmentation implementation

--- examples/java/src/main/java/io/rsocket/pythontest/SimpleRoutingAcceptor.java
+++ examples/java/src/main/java/io/rsocket/pythontest/SimpleRoutingAcceptor.java
@@ -13,38 +13,48 @@ public class SimpleRoutingAcceptor implements SocketAcceptor {
     public Mono<RSocket> accept(ConnectionSetupPayload connectionSetupPayload, RSocket rSocket) {
         return Mono.just(new RoutingRSocketAdapter(new RoutingRSocket() {
             public Mono<Void> fireAndForget(String route, Payload payload) {
-                switch (route) {
-                    case "no_response":
-                        final var str = payload.getDataUtf8();
-                        System.out.println("Received :: " + str);
-                        return Mono.empty();
-                }
+                return Mono.defer(() -> {
+                    switch (route) {
+                        case "no_response":
+                            final var str = payload.getDataUtf8();
+                            System.out.println("Received :: " + str);
+                            return Mono.empty();
+                    }
 
-                return RoutingRSocket.super.fireAndForget(route, payload);
+                    return RoutingRSocket.super.fireAndForget(route, payload);
+                });
             }
 
             public Mono<Payload> requestResponse(String route, Payload payload) {
-                switch (route) {
-                    case "single_request":
-                        return Mono.just(DefaultPayload.create("single_response"));
-                    case "large_request":
-                        return Mono.just(DefaultPayload.create(payload.getDataUtf8()));
-                    case "large_data":
-                        return Mono.just(DefaultPayload.create(Fixtures.largeData()));
-                }
+                return Mono.defer(() -> {
+                    switch (route) {
+                        case "single_request":
+                            return Mono.just(DefaultPayload.create("single_response"));
+                        case "large_request":
+                            if (!payload.getDataUtf8().equals(Fixtures.largeData())) {
+                                return Mono.error(new Exception("Invalid request data"));
+                            }
 
-                return RoutingRSocket.super.requestResponse(route, payload);
+                            return Mono.just(DefaultPayload.create(payload.getDataUtf8()));
+                        case "large_data":
+                            return Mono.just(DefaultPayload.create(Fixtures.largeData()));
+                    }
+
+                    return RoutingRSocket.super.requestResponse(route, payload);
+                });
             }
 
             public Flux<Payload> requestStream(String route, Payload payload) {
-                switch (route) {
-                    case "stream":
-                        return Flux.range(0, 3)
-                                .map(index -> "Item on channel: " + index)
-                                .map(DefaultPayload::create);
-                }
+                return Flux.defer(() -> {
+                    switch (route) {
+                        case "stream":
+                            return Flux.range(0, 3)
+                                    .map(index -> "Item on channel: " + index)
+                                    .map(DefaultPayload::create);
+                    }
 
-                return RoutingRSocket.super.requestStream(route, payload);
+                    return RoutingRSocket.super.requestStream(route, payload);
+                });
             }
         }));
     }
