0067d1f0a3f7e2436f21afe262ab0d22b2c88d2f
Gabriel Shaar <workofishi@pm.me>
2022-10-29 21:28:30 +0300

    refactored test examples. upgrade java server version to 1.1.3

--- examples/test_examples.py
+++ examples/test_examples.py
@@ -6,123 +6,80 @@ from time import sleep
 import pytest
 
 
-@pytest.mark.timeout(12)
-def test_simple_client_server(unused_tcp_port):
-    pid = os.spawnlp(os.P_NOWAIT, 'python3', 'python3', './server.py', str(unused_tcp_port))
+@pytest.mark.timeout(20)
+@pytest.mark.parametrize('server_cli, client_cli', (
+        (
+                lambda unused_tcp_port: os.spawnlp(os.P_NOWAIT, 'python3', 'python3', './server.py',
+                                                   str(unused_tcp_port)),
+                lambda unused_tcp_port: subprocess.Popen(['python3', './client.py', str(unused_tcp_port)])
+        ),
+        (
+                lambda unused_tcp_port: os.spawnlp(os.P_NOWAIT, 'python3', 'python3', './server_quic.py',
+                                                   str(unused_tcp_port)),
+                lambda unused_tcp_port: subprocess.Popen(['python3', './client_quic.py', str(unused_tcp_port)])
+        ),
+        (
+                lambda unused_tcp_port: os.spawnlp(os.P_NOWAIT, 'python3', 'python3', './server_with_routing.py',
+                                                   '--port', str(unused_tcp_port)),
+                lambda unused_tcp_port: subprocess.Popen(['python3', './client_with_routing.py', str(unused_tcp_port)])
+        ),
+        (
+                lambda unused_tcp_port: run_java_class('io.rsocket.pythontest.ServerWithFragmentation',
+                                                       unused_tcp_port),
+                lambda unused_tcp_port: subprocess.Popen(['python3', './client_with_routing.py', str(unused_tcp_port)])
+        ),
+        (
+                lambda unused_tcp_port: os.spawnlp(os.P_NOWAIT, 'python3', 'python3', './server_with_routing.py',
+                                                   '--port', str(unused_tcp_port)),
+                lambda unused_tcp_port: subprocess.Popen(['python3', './client_rx.py', str(unused_tcp_port)])
+        ),
+        (
+                lambda unused_tcp_port: os.spawnlp(os.P_NOWAIT, 'python3', 'python3', './server_aiohttp_websocket.py',
+                                                   str(unused_tcp_port)),
+                lambda unused_tcp_port: subprocess.Popen(['python3', './client_websocket.py', str(unused_tcp_port)])
+        ),
+        (
+                lambda unused_tcp_port: os.spawnlp(os.P_NOWAIT, 'python3', 'python3', 'server_aiohttp_websocket.py',
+                                                   '--port', str(unused_tcp_port),
+                                                   '--with-ssl'),
+                lambda unused_tcp_port: subprocess.Popen(
+                    ['python3', './client_websocket.py', '--port', str(unused_tcp_port), '--with-ssl'])
+        ),
+        (
+                lambda unused_tcp_port: os.spawnlp(os.P_NOWAIT, 'python3', 'python3', './server_quart_websocket.py',
+                                                   '--port', str(unused_tcp_port)),
+                lambda unused_tcp_port: subprocess.Popen(
+                    ['python3', './client_websocket.py', '--port', str(unused_tcp_port)])
+        ),
+        (
+                lambda unused_tcp_port: run_java_class('io.rsocket.pythontest.Server', unused_tcp_port),
+                lambda unused_tcp_port: subprocess.Popen(
+                    ['python3', './run_against_example_java_server.py', str(unused_tcp_port)])
+        ),
+        (
+                lambda unused_tcp_port: os.spawnlp(os.P_NOWAIT, 'python3', 'python3', './server_with_routing.py',
+                                                   '--port', str(unused_tcp_port)),
+                lambda unused_tcp_port: subprocess.Popen(['java',
+                                                          '-cp', 'java/target/rsocket-examples-1.jar',
+                                                          'io.rsocket.pythontest.Client',
+                                                          '%d' % unused_tcp_port])
+        ),
+        (
+                lambda unused_tcp_port: os.spawnlp(os.P_NOWAIT, 'python3', 'python3', './server_with_lease.py',
+                                                   str(unused_tcp_port)),
+                lambda unused_tcp_port: subprocess.Popen(['java',
+                                                          '-cp', 'java/target/rsocket-examples-1.jar',
+                                                          'io.rsocket.pythontest.ClientWithLease',
+                                                          '%d' % unused_tcp_port])
+        ),
+))
+def test_client_server_combinations(unused_tcp_port, server_cli, client_cli):
+    pid = server_cli(unused_tcp_port)
 
     try:
         sleep(2)
-        client = subprocess.Popen(['python3', './client.py', str(unused_tcp_port)])
-        client.wait(timeout=10)
-
-        assert client.returncode == 0
-    finally:
-        os.kill(pid, signal.SIGTERM)
-
-
-@pytest.mark.timeout(12)
-def test_quic_client_server(unused_tcp_port):
-    pid = os.spawnlp(os.P_NOWAIT, 'python3', 'python3', './server_quic.py', str(unused_tcp_port))
-
-    try:
-        sleep(2)
-        client = subprocess.Popen(['python3', './client_quic.py', str(unused_tcp_port)])
-        client.wait(timeout=10)
-
-        assert client.returncode == 0
-    finally:
-        os.kill(pid, signal.SIGTERM)
-
-
-@pytest.mark.timeout(30)
-def test_client_server_with_routing(unused_tcp_port):
-    pid = os.spawnlp(os.P_NOWAIT, 'python3', 'python3', './server_with_routing.py', '--port', str(unused_tcp_port))
-
-    try:
-        sleep(2)
-        client = subprocess.Popen(['python3', './client_with_routing.py', str(unused_tcp_port)])
+        client = client_cli(unused_tcp_port)
         client.wait(timeout=20)
-
-        assert client.returncode == 0
-    finally:
-        os.kill(pid, signal.SIGTERM)
-
-
-@pytest.mark.timeout(30)
-def test_client_java_server_with_routing_and_fragmentation(unused_tcp_port):
-    pid = run_java_class('io.rsocket.pythontest.ServerWithFragmentation', unused_tcp_port)
-
-    try:
-        sleep(2)
-        client = subprocess.Popen(['python3', './client_with_routing.py', str(unused_tcp_port)])
-        client.wait(timeout=20)
-
-        assert client.returncode == 0
-    finally:
-        os.kill(pid, signal.SIGTERM)
-
-
-@pytest.mark.timeout(30)
-def test_rx_client_server_with_routing(unused_tcp_port):
-    pid = os.spawnlp(os.P_NOWAIT, 'python3', 'python3', './server_with_routing.py', '--port', str(unused_tcp_port))
-
-    try:
-        sleep(2)
-        client = subprocess.Popen(['python3', './client_rx.py', str(unused_tcp_port)])
-        client.wait(timeout=20)
-
-        assert client.returncode == 0
-    finally:
-        os.kill(pid, signal.SIGTERM)
-
-
-def test_client_server_over_websocket_aiohttp(unused_tcp_port):
-    pid = os.spawnlp(os.P_NOWAIT, 'python3', 'python3', './server_aiohttp_websocket.py', str(unused_tcp_port))
-
-    try:
-        sleep(2)
-        client = subprocess.Popen(['python3', './client_websocket.py', str(unused_tcp_port)])
-        client.wait(timeout=3)
-
-        assert client.returncode == 0
-    finally:
-        os.kill(pid, signal.SIGTERM)
-
-
-def test_client_server_over_websocket_secure_aiohttp(unused_tcp_port):
-    pid = os.spawnlp(os.P_NOWAIT, 'python3', 'python3', 'server_aiohttp_websocket.py', '--port', str(unused_tcp_port),
-                     '--with-ssl')
-
-    try:
-        sleep(2)
-        client = subprocess.Popen(['python3', './client_websocket.py', '--port', str(unused_tcp_port), '--with-ssl'])
-        client.wait(timeout=3)
-
-        assert client.returncode == 0
-    finally:
-        os.kill(pid, signal.SIGTERM)
-
-
-def test_client_server_over_websocket_quart(unused_tcp_port):
-    pid = os.spawnlp(os.P_NOWAIT, 'python3', 'python3', './server_quart_websocket.py', '--port', str(unused_tcp_port))
-
-    try:
-        sleep(2)
-        client = subprocess.Popen(['python3', './client_websocket.py', '--port', str(unused_tcp_port)])
-        client.wait(timeout=3)
-
-        assert client.returncode == 0
-    finally:
-        os.kill(pid, signal.SIGTERM)
-
-
-def test_client_java_server(unused_tcp_port):
-    pid = run_java_class('io.rsocket.pythontest.Server', unused_tcp_port)
-
-    try:
-        sleep(2)
-        client = subprocess.Popen(['python3', './run_against_example_java_server.py', str(unused_tcp_port)])
-        client.wait(timeout=6)
 
         assert client.returncode == 0
     finally:
@@ -133,34 +90,3 @@ def run_java_class(java_class: str, unused_tcp_port: int):
     return os.spawnlp(os.P_NOWAIT, 'java', 'java',
                       '-cp', 'java/target/rsocket-examples-1.jar', java_class,
                       '%d' % unused_tcp_port)
-
-
-def test_java_client_server(unused_tcp_port):
-    pid = os.spawnlp(os.P_NOWAIT, 'python3', 'python3', './server_with_routing.py', '--port', str(unused_tcp_port))
-
-    try:
-        sleep(2)
-        client = subprocess.Popen(['java',
-                                   '-cp', 'java/target/rsocket-examples-1.jar', 'io.rsocket.pythontest.Client',
-                                   '%d' % unused_tcp_port])
-        client.wait(timeout=6)
-
-        assert client.returncode == 0
-    finally:
-        os.kill(pid, signal.SIGTERM)
-
-
-@pytest.mark.timeout(30)
-def test_java_client_server_lease(unused_tcp_port):
-    pid = os.spawnlp(os.P_NOWAIT, 'python3', 'python3', './server_with_lease.py', str(unused_tcp_port))
-
-    try:
-        sleep(2)
-        client = subprocess.Popen(['java',
-                                   '-cp', 'java/target/rsocket-examples-1.jar', 'io.rsocket.pythontest.ClientWithLease',
-                                   '%d' % unused_tcp_port])
-        client.wait(timeout=15)
-
-        assert client.returncode == 0
-    finally:
-        os.kill(pid, signal.SIGTERM)
