8293e7e22490c50fcd76abd033a6cb0f18ade617
jell-o-fishi <jellofishi@pm.me>
2022-11-17 16:44:36 +0200

    test server fixture close on finally

--- tests/tools/fixtures_aiohttp.py
+++ tests/tools/fixtures_aiohttp.py
@@ -1,13 +1,6 @@
 import asyncio
-from asyncio import Event
-from contextlib import asynccontextmanager
-from typing import Optional
 
 import pytest
-
-from rsocket.rsocket_base import RSocketBase
-from rsocket.transports.aiohttp_websocket import websocket_client, websocket_handler_factory
-from tests.rsocket.helpers import assert_no_open_streams
 
 
 @pytest.fixture
@@ -16,19 +9,17 @@ def aiohttp_raw_server(event_loop: asyncio.BaseEventLoop, unused_tcp_port):
 
     servers = []
 
-    async def go(handler):
-        server = RawTestServer(handler, port=unused_tcp_port)
-        await server.start_server()
-        servers.append(server)
-        return server
+    try:
+        async def go(handler):
+            server = RawTestServer(handler, port=unused_tcp_port)
+            await server.start_server()
+            servers.append(server)
+            return server
 
-    yield go
+        yield go
+    finally:
+        async def finalize() -> None:
+            while servers:
+                await servers.pop().close()
 
-    async def finalize() -> None:
-        while servers:
-            await servers.pop().close()
-
-    event_loop.run_until_complete(finalize())
-
-
-
+        event_loop.run_until_complete(finalize())
