d9b5f2d70eae529118f479196f0d1c6f939c94ac
jell-o-fishi <jellofishi@pm.me>
2022-12-09 19:19:36 +0200

    added unit tests for empty routes

--- tests/rsocket/test_request_router.py
+++ tests/rsocket/test_request_router.py
@@ -1,5 +1,6 @@
 import pytest
 
+from rsocket.exceptions import RSocketEmptyRoute
 from rsocket.helpers import create_future
 from rsocket.local_typing import Awaitable
 from rsocket.payload import Payload
@@ -15,5 +16,19 @@ async def test_request_router_exception_on_duplicate_route_with_same_type():
             return create_future()
 
         @router.response('path1')
+        async def request_response2(payload, composite_metadata) -> Awaitable[Payload]:
+            return create_future()
+
+
+async def test_request_router_disallow_empty_routes():
+    router = RequestRouter()
+
+    with pytest.raises(RSocketEmptyRoute):
+        @router.response('')
+        async def request_response(payload, composite_metadata) -> Awaitable[Payload]:
+            return create_future()
+
+    with pytest.raises(RSocketEmptyRoute):
+        @router.response(None)
         async def request_response2(payload, composite_metadata) -> Awaitable[Payload]:
             return create_future()
