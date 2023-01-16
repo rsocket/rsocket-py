d415fd75e62e562318ae042a97b6503347c58ce6
jell-o-fishi <111562631+jell-o-fishi@users.noreply.github.com>
2023-01-16 13:25:44 +0200

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as fd:
 
 setup(
     name='rsocket',
-    version='0.4.7',
+    version='0.4.8',
     description='Python RSocket library',
     long_description=long_description,
     long_description_content_type='text/markdown',
