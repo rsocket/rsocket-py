f001953e06782e2ce3472c8e602b59f006144846
Frank GooDer <frankgooder@centrum.cz>
2023-07-18 13:32:26 +0200

    Reworked unit test

--- tests/rsocket/test_extentions.py
+++ tests/rsocket/test_extentions.py
@@ -1,5 +1,11 @@
+import pytest
+import struct
+
+from rsocket.exceptions import RSocketError
 from rsocket.extensions.authentication import AuthenticationBearer, AuthenticationSimple
+from rsocket.extensions.mimetypes import WellKnownMimeTypes
 from rsocket.extensions.routing import RoutingMetadata
+from rsocket.extensions.tagging import TaggingMetadata
 
 
 def test_authentication_bearer():
@@ -39,3 +45,32 @@ def test_routing():
     parsed.parse(data)
 
     assert parsed == routing
+
+
+def test_tagging_metadata_serialize_max_length():
+    tag = 's' * 255
+    meta = TaggingMetadata(WellKnownMimeTypes.MESSAGE_RSOCKET_ROUTING, [tag])
+
+    serialized = meta.serialize()
+
+    length = struct.pack('>B', len(tag))
+    assert length + bytes(tag, 'utf-8') == serialized
+
+
+def test_tagging_metadata_serialize_exception_length():
+    tag = 's' * 256
+    meta = TaggingMetadata(WellKnownMimeTypes.MESSAGE_RSOCKET_ROUTING, [tag])
+
+    with pytest.raises(RSocketError) as e_info:
+        meta.serialize()
+
+    assert e_info.match(f'Tag length longer than 255 characters: "b\'{tag}\'"')
+
+
+def test_tagging_metadata_parse():
+    meta = TaggingMetadata(WellKnownMimeTypes.MESSAGE_RSOCKET_ROUTING)
+    tag = 's' * 255
+    length = struct.pack('>B', len(tag))
+
+    meta.parse(length + bytes(tag, 'utf-8'))
+    assert tag == meta.tags[0].decode()
