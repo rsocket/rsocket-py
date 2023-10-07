b50fa4a2e1633a07bc9dd135aa70163a1bab2baf
jell-o-fishi <jellofishi@pm.me>
2023-10-07 19:56:27 +0300

    added streaming example

--- examples/graphql/client_graphql.py
+++ examples/graphql/client_graphql.py
@@ -2,16 +2,12 @@ import asyncio
 import logging
 import sys
 
-import aiohttp
 from gql import gql, Client
 
-from rsocket.extensions.helpers import route, composite
 from rsocket.extensions.mimetypes import WellKnownMimeTypes
 from rsocket.graphql.rsocket_transport import RSocketTransport
 from rsocket.helpers import single_transport_provider
-from rsocket.payload import Payload
 from rsocket.rsocket_client import RSocketClient
-from rsocket.transports.aiohttp_websocket import TransportAioHttpClient
 from rsocket.transports.tcp import TransportTCP
 
 
@@ -33,46 +29,20 @@ type Greeting {
             transport=RSocketTransport(client),
         )
 
-        # response = await client.request_response(Payload(metadata=composite(route('ping'))))
-        #
-        # assert response.data == b'pong'
-
         await greeting(graphql)
 
-        # await echo(graphql)
-
-        # await subscription(graphql)
+        await subscription(graphql)
 
 
 async def subscription(graphql: Client):
     async for response in graphql.subscribe_async(
             document=gql("""
-                subscription multipleGreetings {
-                    multipleGreetings
+                subscription {
+                    greetings {message}
                 }
                 """),
             get_execution_result=True):
         print(response.data)
-
-
-async def echo(graphql: Client):
-    message = 'and now for something completely different'
-
-    response = await graphql.execute_async(
-        document=gql("""
-                query echo($input: String) {
-                    echo(input: $input) {
-                      message
-                    }
-                  }
-                """),
-        variable_values={'input': message},
-        get_execution_result=True
-    )
-
-    assert response.data['echo']['message'] == message
-
-    print(response.data)
 
 
 async def greeting(graphql: Client):
